2fdac11e01366987cac355357ee9b2d8e5c54f97
bLuDd1 <dtavluj@gmail.com>
2023-12-19 02:20:47 +0200

    Changed endpoints for user

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+
 import src.views
 import src.resources.user
 import src.resources.record
--- src/resources/user.py
+++ src/resources/user.py
@@ -1,36 +1,50 @@
 import uuid
 from flask import jsonify, request
-from src import app
+from src import app, db
+from src.models import UserModel
+from src.schemas import UserSchema
+from marshmallow import ValidationError
 
-users = {}
+user_schema = UserSchema()
+
 
 @app.get('/user/<user_id>')
 def get_user(user_id):
-    if user_id not in users:
-        return jsonify(error=f'User with {user_id} id does not exist'), 404
-    else:
-        user = users[user_id]
-        return jsonify(user)
+    user = UserModel.query.get(user_id)
+    try:
+        return jsonify(user_schema.dump(user)), 200
+    except ValidationError as err:
+        return jsonify(err.messages), 400
 
 
 @app.delete('/user/<user_id>')
 def delete_user(user_id):
-    if user_id not in users:
-        return jsonify(error=f'User with {user_id} id does not exist'), 404
-    else:
-        del users[user_id]
-        return f"User deleted by {user_id} id"
+    user = UserModel.query.get(user_id)
+    try:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify(user_schema.dump(user)), 200
+    except ValidationError as err:
+        return jsonify(err.messages), 400
 
 
 @app.post('/user')
 def create_user():
-    user_name = request.args.get("name")
-    user_id = uuid.uuid4().hex
-    user = {"id": user_id, "name": user_name}
-    users[user_id] = user
+    try:
+        data = user_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    data['id'] = uuid.uuid4().hex
+    user = UserModel(**data)
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        return jsonify(error=str(e)), 400
     return jsonify(user)
 
 
 @app.get('/users')
 def get_users():
-    return list(users.values())
+    users = UserModel.query.all()
+    return jsonify(user_schema.dump(users, many=True))
