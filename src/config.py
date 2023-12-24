d1928702bdf2f57305f5b5ead524567b7d14d548
bLuDd1 <dtavluj@gmail.com>
2023-12-24 20:36:14 +0200

    Try to run in render.com

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-PROPAGATE_EXCEPTIONS = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root123@db:5432/postgres'
+#PROPAGATE_EXCEPTIONS = True
+SQLALCHEMY_DATABASE_URI = 'postgres://backend_lab3_user:e6Hdn4W3l9P1z8xct3uj3hiuoiQWnt7l@dpg-cm46ggocmk4c73ci0bag-a.oregon-postgres.render.com/backend_lab3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
